7ba5a34160399a7c052148d74c3ab4921cb23e95
unknown <marketing.KevinConti@gmail.com>
2020-07-23 10:10:12 -0400

    Initial commit

---
+++ Node.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tree
+{
+    /// <summary>
+    /// Node is a recursive data structure, meaning that a Node will have references to other nodes
+    /// Note that the "edges" are implicit and we don't need to directly define them
+    /// </summary>
+    public class Node<T> // The T means that it can hold any type inside it
+    {
+        public T Value { get; set; }
+
+        /// <summary>
+        /// The constructor function for Node
+        /// </summary>
+        /// <param name="value">The Value to put inside the Node</param>
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// The parent node to this node
+        /// </summary>
+        public Node<T> Parent { get; set; }
+
+        /// <summary>
+        /// All the children Nodes of this Node, if any
+        /// </summary>
+        public List<Node<T>> Children { get; set; }
+
+
+
+
+        // The purpose of the below methods are so that we don't have to call myNode.Children all the time
+
+        /// <summary>
+        /// A convenience function that returns whether or not this node has any children.
+        /// </summary>
+        /// <returns></returns>
+        public bool HasChildren()
+        {
+            throw new NotImplementedException("Nikki should implement this method");
+        }
+
+        /// <summary>
+        /// Adds a child to the Children property
+        /// </summary>
+        /// <param name="node">The node to add to the children property</param>
+        public void AddChild(Node<T> node)
+        {
+            throw new NotImplementedException("Nikki should implement this method");
+        }
+
+        public void GetChild(int index)
+        {
+            throw new NotImplementedException("Nikki should implement this method");
+        }
+
+    }
+}
---
+++ Tree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class Tree<T>
+    {
+        /// <summary>
+        /// The root, or top-level node, for this data structure
+        /// </summary>
+        public Node<T> RootNode { get; set; }
+
+        /// <summary>
+        /// The tree constructor, which takes the value for the RootNode
+        /// </summary>
+        /// <param name="value">The value that goes in the RootNode</param>
+        public Tree(T value)
+        {
+            RootNode = new Node<T>(value);
+        }
+
+        /// <summary>
+        /// An alternate constructor which takes the RootNode of the tree
+        /// </summary>
+        /// <param name="node">The node to become the RootNode</param>
+        public Tree(Node<T> node)
+        {
+            RootNode = node;
+        }
+
+        /// <summary>
+        /// Returns how "deep" a tree is. For example, if the RootNode has Children, and one of those children has Children, the depth = 3
+        /// This will require you to write either a breadth-first or depth-first algorithm. Happy to work with you on this.
+        /// </summary>
+        /// <returns>Ant int that represents the depth of the tree</returns>
+        public int Depth()
+        {
+            throw new NotImplementedException("Nikki should implement this method");
+        }
+
+        /// <summary>
+        /// Prints out the value of each Node in the tree. If you wrote Depth using breadth-first search, then we'll do this one with depth-first search, or vice-versa.
+        /// </summary>
+        public void PrintAllValues()
+        {
+            throw new NotImplementedException("Nikki should implement this method");
+        }
+    }
+}
